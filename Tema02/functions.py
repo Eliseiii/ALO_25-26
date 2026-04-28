5d52553738ed12ee15822bd19cb3e388edcda632
Eliseiii <chriac.elisei@gmail.com>
2026-04-28 10:31:29 +0300

    modificat la 28/04/2026

--- Tema02/functions.py
+++ Tema02/functions.py
@@ -1,5 +1,10 @@
 def f(x):
-    return x**3+2*x**2-3*x+4
+    return (1/3)*x**3-2*x**2+2*x+3
+
+def derivata_inf(x):
+    h=1e-6
+    return (3*f(x)-4*f(x-h)+f(x-2*h))/(2*h)
+
 
 def derivata(x):
    h=1e-6
@@ -21,3 +26,16 @@ def delta(x, y):
             return 1e-5
     
     return (x-y)*derivata(x)/numitor
+
+def delta_inf(x, y):
+
+    h=1e-5
+    numitor = derivata_inf(x) - derivata_inf(y)
+    
+    if abs(numitor) < h:
+        if abs(derivata_inf(x)) < h/100:
+            return 0.0
+        else:
+            return 1e-5
+    
+    return (x-y)*derivata_inf(x)/numitor
--- Tema02/main.py
+++ Tema02/main.py
@@ -8,6 +8,8 @@ def metoda_secantei():
     kmax=1000
     x0=random.uniform(0.1, 10.0)
     x1=random.uniform(0.1, 10.0)
+    x00=x0
+    x11=x1
 
     k=0
     de=1.0
@@ -20,7 +22,7 @@ def metoda_secantei():
         de = functions.delta(x1, x0)
 
         if de == 0.0:
-             break
+            break
 
         x=x1 - de
 
@@ -36,11 +38,48 @@ def metoda_secantei():
 
     if abs(de)<eps or de==0.0:
         if functions.derivata_sec(x1) > 0:
-            print(f"Metoda secantei a convergat la solutia: {x1:.6f} in {k} iteratii.")
+            print(f"Metoda secantei a convergat la solutia: {x1:.16f} in {k} iteratii.")
         else:
             print("Metoda secantei nu a convergat.")
     else:
         print(f"Algoritmul a eșuat (Divergență sau kmax atins).")
+
+
+
+    # Metoda secantei cu derivata neeficienta
+    
+    k1=0
+    dee=1.0
+    while True:
+
+        dee = functions.delta_inf(x11, x00)
+
+        if dee == 0.0:
+            break
+
+        xx=x11 - dee
+
+        if abs(dee)<eps:
+            x11=xx
+            break
+
+        x00, x11 = x11, xx
+        k1=k1+1
+
+        if k1>=kmax or abs(x11)>10**8:
+            break
+
+    if abs(dee)<eps or dee==0.0:
+        if functions.derivata_sec(x11) > 0:
+            print(f"Metoda secantei cu derivata neeficienta a convergat la solutia: {x11:.16f} in {k1} iteratii.")
+        else:
+            print("Metoda secantei cu derivata neeficienta nu a convergat.")
+    else:
+        print(f"Algoritmul a eșuat (Divergență sau kmax atins).")
+
+    print(f"Diferenta dintre solutiile obtinute: {abs(x11-x1):.16e}")
+
+
 
 if __name__ == "__main__":
     metoda_secantei()
